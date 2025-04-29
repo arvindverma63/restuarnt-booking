49379ca3d4f722b5b114f02b326eb3bd6065ffa7
ARVIND_VERMA <arvindverma630635@gmail.com>
2025-04-29 11:53:52 +0530

    upload first angular project

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [RouterLink,NgIf],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  isOpen = false;
+
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+  }
+}
