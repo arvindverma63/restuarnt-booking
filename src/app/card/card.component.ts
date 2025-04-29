60a557f05c1eb4d4b0b8d5609f03456659ca0866
ARVIND_VERMA <arvindverma630635@gmail.com>
2025-04-29 12:30:43 +0530

    update

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+    items = ['https://m.media-amazon.com/images/I/61gDg-vFhzL.jpg',
+      'https://cdn.jiostore.online/v2/jmd-asp/jdprod/wrkr/products/pictures/item/free/original/fKkunRGFE4-redmi-14c-5g-494493753-i-1-1200wx1200h.jpeg','https://exstatic-in.iqoo.com/Oz84QB3Wo0uns8j1/in/1743578201463/a3d528f779699a6d0c626ff0a71696c1.png_w860-h860.webp'
+      ,'https://5.imimg.com/data5/SELLER/Default/2024/4/406768556/GX/KV/JQ/3198250/mobile-phones-500x500.jpg','https://lirp.cdn-website.com/8ac3ee00/dms3rep/multi/opt/durasport5g-front-all-phones-640w.png']
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../hero/hero.component';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeroComponent,CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
